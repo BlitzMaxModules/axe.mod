1e977b1af867a77cb313bf195ad9ae49889fc835
nitrologic <nitrologic@548b755b-aa20-0410-b24a-7f7e2b255a79>
2009-05-26 10:48:46 +0000

    skid: first build of new axe.v8 module
    git-svn-id: http://max-edit.googlecode.com/svn/trunk/mod/axe.mod@48 548b755b-aa20-0410-b24a-7f7e2b255a79

---
+++ v8.mod/b8.cpp
@@ -0,0 +1,46 @@
+// b8.cpp
+// blitzmax V8 stub
+// by simon armstrong
+// nitrologic@gmail.com
+
+#include <v8.h>
+
+using namespace v8;
+
+extern "C" int v8main(char* script);
+
+int v8main(char *js) {
+ 
+ // Create a stack-allocated handle scope.
+  HandleScope handle_scope;
+
+  // Create a new context.
+  Persistent<Context> context = Context::New();
+  
+  // Enter the created context for compiling and
+  // running the hello world script. 
+  Context::Scope context_scope(context);
+
+  // Create a string containing the JavaScript source code.
+  Handle<String> source = String::New( js );  //"'Hello' + ', World!'");
+
+  // Compile the source code.
+  Handle<Script> script = Script::Compile(source);
+  
+  // Run the script to get the result.
+  Handle<Value> result = script->Run();
+  
+  // Dispose the persistent context.
+  context.Dispose();
+
+  // Convert the result to an ASCII string and print it.
+
+  String::AsciiValue ascii(result);
+
+  if(*ascii){
+    printf("%s\n", *ascii);
+  }
+  
+  return 0;
+}
+
---
+++ v8.mod/testv8.bmx
@@ -0,0 +1,5 @@
+' testv8
+
+Import axe.v8
+
+v8("a=22;b=a*55;")
